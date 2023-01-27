9cfae4ce327ef36c7fc0625de1ac9e620ee26a19
DIMA_w <oddff@mail.ru>
2023-01-27 15:36:26 +0300

    Проработка клавишь

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,38 @@
-function btn_x2() {
+function enterText(el) {
+  document.getElementById("text").value += el;
+}
+
+function enterText_1(el) {
   document.getElementById("newFormula").value += document.getElementById("text").value;
-  console.log(Text.value);
+  document.getElementById("newFormula").value += el;
   document.getElementById("text").value = "";
+}
+
+function addStartFormul(el) {
+  document.getElementById("newFormula").value = "0" + el;
+}
+
+function btn_number(el) {
+  enterText(el);
+}
+
+function btn_dot() {
+  const Text = document.getElementById("text").value;
+  if (Text.includes(".", 0) !== true) {
+    enterText(".");
+  }
+
+  if (Text[0] === undefined) {
+    document.getElementById("text").value = "0.";
+  }
+}
+function btn_addtion() {
+  const Text = document.getElementById("text").value;
+  if (Text[0] === undefined) {
+    addStartFormul("+");
+  }
+
+  if (Text[0] !== undefined) {
+    enterText_1("+");
+  }
 }
